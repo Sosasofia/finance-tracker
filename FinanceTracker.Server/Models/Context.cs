0b99b9d2465864a8e12de10138f2ca2bb2e0e994
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-04-03 11:46:47 -0300

    Add transactions and related tables with relationships
    - Added migration to create tables for Category, PaymentMethod,
    Transaction, Installment, and Reimburstment entities.
    - Created new entity classes and
    established relationships between Transaction and other related entities.
    - Updated Context.cs to include new entities and configure their relationships.

--- FinanceTracker.Server/Models/Context.cs
+++ FinanceTracker.Server/Models/Context.cs
@@ -1,4 +1,5 @@
-﻿using FinanceTracker.Server.Services.Models;
+﻿using FinanceTracker.Server.Models.Entities;
+using FinanceTracker.Server.Services.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinanceTracker.Server.Models
@@ -10,12 +11,46 @@ namespace FinanceTracker.Server.Models
         public Context(DbContextOptions<Context> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<Reimburstment> Reimburstments { get; set; }
+        public DbSet<Installment> Installments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Reimburstment>()
+                .HasOne(r => r.Transaction)
+                .WithOne(t => t.Reimburstment)
+                .HasForeignKey<Reimburstment>(r => r.TransactionId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Installment>()
+                .HasOne(i => i.Transaction)
+                .WithMany(t => t.InstallmentsList)
+                .HasForeignKey(i => i.TransactionId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+           modelBuilder.Entity<Reimburstment>()
+                .Property(r => r.Amount)
+                .HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Transaction>()
+                .Property(t => t.Amount)
+                .HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Installment>()
+                .Property(r => r.Amount)
+                .HasColumnType("decimal(18,2)");
+
             modelBuilder.Entity<User>().ToTable("user");
+            modelBuilder.Entity<Transaction>().ToTable("transaction");
+            modelBuilder.Entity<Category>().ToTable("category");
+            modelBuilder.Entity<PaymentMethod>().ToTable("payment_method");
+            modelBuilder.Entity<Reimburstment>().ToTable("reimburstment");
+            modelBuilder.Entity<Installment>().ToTable("installment");
         }
     }
 }
---
+++ FinanceTracker.Server/Models/Entities/Installment.cs
@@ -0,0 +1,18 @@
+﻿namespace FinanceTracker.Server.Models.Entities
+{
+    public class Installment
+    {
+        public Guid Id { get; set; }
+        public int InstallmentNumber { get; set; } 
+        public decimal Amount { get; set; } 
+        public DateTime DueDate { get; set; } 
+        public DateTime? PaymentDate { get; set; }
+        public bool IsPaid { get; set; } = false;
+
+        // Foreign keys
+        public Guid TransactionId { get; set; }
+
+        // Nav properties
+        public Transaction Transaction { get; set; }
+    }
+}
