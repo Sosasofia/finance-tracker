9e2d4c6c19d82866ac818d285646d5a129919ba1
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-04-03 11:55:21 -0300

    Add Type column to transactions and update related code
    - Added migration to add `Type` column to `transaction` table.
    - Updated `Transaction` class to include `Type` property with default `TransactionType.Expense`.

--- FinanceTracker.Server/Migrations/20250403145253_AddTransactionTypeToTransactionTable.Designer.cs
+++ FinanceTracker.Server/Migrations/20250403145253_AddTransactionTypeToTransactionTable.Designer.cs
@@ -4,6 +4,7 @@ using FinanceTracker.Server.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FinanceTracker.Server.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250403145253_AddTransactionTypeToTransactionTable")]
+    partial class AddTransactionTypeToTransactionTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
