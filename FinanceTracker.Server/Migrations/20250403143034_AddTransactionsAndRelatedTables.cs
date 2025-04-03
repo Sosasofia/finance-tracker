b71162de917ba4a73a655340fe8cfdbfc94ee993
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-04-03 11:46:47 -0300

    Add transactions and related tables with relationships
    - Added migration to create tables for Category, PaymentMethod,
    Transaction, Installment, and Reimburstment entities.
    - Created new entity classes and
    established relationships between Transaction and other related entities.
    - Updated Context.cs to include new entities and configure their relationships.

---
+++ FinanceTracker.Server/Migrations/20250403143034_AddTransactionsAndRelatedTables.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceTracker.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTransactionsAndRelatedTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "category",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "payment_method",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_payment_method", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transaction",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    BussinessName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReceiptUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ReimburstmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    PaymentMethodId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsCreditCardPurchase = table.Column<bool>(type: "bit", nullable: false),
+                    Installments = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_transaction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_transaction_category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "category",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_transaction_payment_method_PaymentMethodId",
+                        column: x => x.PaymentMethodId,
+                        principalTable: "payment_method",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_transaction_user_UserId",
+                        column: x => x.UserId,
+                        principalTable: "user",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "installment",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InstallmentNumber = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PaymentDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsPaid = table.Column<bool>(type: "bit", nullable: false),
+                    TransactionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_installment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_installment_transaction_TransactionId",
+                        column: x => x.TransactionId,
+                        principalTable: "transaction",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reimburstment",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TransactionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reimburstment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_reimburstment_transaction_TransactionId",
+                        column: x => x.TransactionId,
+                        principalTable: "transaction",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_installment_TransactionId",
+                table: "installment",
+                column: "TransactionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reimburstment_TransactionId",
+                table: "reimburstment",
+                column: "TransactionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transaction_CategoryId",
+                table: "transaction",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transaction_PaymentMethodId",
+                table: "transaction",
+                column: "PaymentMethodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transaction_UserId",
+                table: "transaction",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "installment");
+
+            migrationBuilder.DropTable(
+                name: "reimburstment");
+
+            migrationBuilder.DropTable(
+                name: "transaction");
+
+            migrationBuilder.DropTable(
+                name: "category");
+
+            migrationBuilder.DropTable(
+                name: "payment_method");
+        }
+    }
+}
