9084c4b3f688bf66cf2272e36ba1748b27ca451c
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-04-09 16:14:35 -0300

    Add DTOs, response classes, and update mappings
    - Added InstallmentDTO, ReimburstmentDTO, TransactionCreateDTO, InstallmentResponse, and TransactionResponse classes to represent
    various data structures.
    - Updated MappingProfile.cs to include
    mappings for these new DTOs and response classes.
    - Introduced a
    generic Response<T> class for API responses.

---
+++ FinanceTracker.Server/Models/DTOs/InstallmentDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceTracker.Server.Models.DTOs
+{
+    public class InstallmentDTO
+    {
+        public int Number { get; set; }
+        public int? Interest { get; set; }
+    }
+}
