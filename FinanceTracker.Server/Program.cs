d458d419df30101d32b5a1613a34149b5d44dc22
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-04-09 16:23:43 -0300

    Add SeedData class and configure JSON options
    - Added `SeedData.cs` with `SeedData` class
    - Implemented `SeedData.Initialize` method to seed initial data for `Categories` and `PaymentMethods`.
    - Updated `Program.cs` to configure JSON serialization with `JsonStringEnumConverter`.

--- FinanceTracker.Server/Program.cs
+++ FinanceTracker.Server/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,10 @@ builder.Logging.AddConsole();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -63,6 +67,23 @@ builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var env = services.GetRequiredService<IWebHostEnvironment>();
+
+    if(env.IsDevelopment())
+    {
+        var context = services.GetRequiredService<Context>();
+
+        // DB exists
+        context.Database.Migrate();
+
+        // Seed data
+        SeedData.Initialize(context);
+    }
+}
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
