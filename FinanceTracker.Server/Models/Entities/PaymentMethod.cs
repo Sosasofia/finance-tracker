33dbcc24052b45c587dcba1b0b8a97b889da3e4b
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-04-09 15:51:50 -0300

    Model update: cleaned up unused fields and introduced new property, applied corresponding migration to db
    Updated PaymentMethod.cs:
    - Added Type property to PaymentMethod class
    
    Updated Transaction.cs:
    - Removed ReimburstmentId property from Transaction class

--- FinanceTracker.Server/Models/Entities/PaymentMethod.cs
+++ FinanceTracker.Server/Models/Entities/PaymentMethod.cs
@@ -4,5 +4,6 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Type { get; set; } // credit card, debit card, cash, bank transfer, etc.
     }
 }
