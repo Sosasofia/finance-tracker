a77fa8d1896969d31dba875b48d1594c70dd5422
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-04-11 17:43:47 -0300

    Refactor AuthController and update auth models
    - Refactored `AuthController` to use `AuthRequest` and `AuthResponse` models, improving code structure and readability
    - Removed `ILogger<AuthController>` dependency.

---
+++ FinanceTracker.Server/Models/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace FinanceTracker.Server.Models.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Role { get; set; } = "User";
+    }
+}
