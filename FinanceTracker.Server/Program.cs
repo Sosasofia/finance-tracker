b311f1077578d2e8ff0d320d0430f3e0f636252b
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-04-18 21:27:14 -0300

    Update Program.cs

--- FinanceTracker.Server/Program.cs
+++ FinanceTracker.Server/Program.cs
@@ -6,37 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration["ConnectionStrings:FinanceDB"];
 Console.WriteLine($"Connection string: {connectionString}");
 
-if (string.IsNullOrEmpty(connectionString))
-{
-    throw new Exception("Connection string 'FinanceDB' not found.");
-}
 
-try
-{
-    builder.Services.AddDbContext<Context>(options =>
-        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
-    );
-}
-catch (Exception ex)
-{
-    Console.WriteLine("Error al configurar el DbContext:");
-    Console.WriteLine(ex.ToString());
-    throw;  
-}
-
+builder.Services.AddDbContext<Context>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
 
 var app = builder.Build();
-
 
 app.MapGet("/", () => "¡API corriendo desde Railway!");
 
 
-try
-{
-    app.Run();
-}
-catch (Exception ex)
-{
-    Console.WriteLine("Error al ejecutar la app:");
-    Console.WriteLine(ex.ToString());
-}
+app.Run();
