68c41b829c69fe690fdf040d6cb422cda8bd099f
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-04-18 21:05:11 -0300

    Simplify app configuration

--- FinanceTracker.Server/Program.cs
+++ FinanceTracker.Server/Program.cs
@@ -1,45 +1,11 @@
-﻿using FinanceTracker.Server.Models;
-using FinanceTracker.Server.Repositories;
-using FinanceTracker.Server.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+﻿using Microsoft.EntityFrameworkCore;
+using FinanceTracker.Server.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin", policy =>
-    {
-        policy.WithOrigins("https://localhost:57861") 
-              .AllowAnyHeader()  
-              .AllowAnyMethod(); 
-    });
-});
 
-builder.Services.AddControllers().AddJsonOptions(x =>
-{
-    x.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-});
-
-
-// Add services to the container.
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var connectionString = builder.Configuration.GetConnectionString("FinanceDB");
-
-// Log the connection string to the console
-Console.WriteLine(builder.Configuration.GetConnectionString("FinanceDB"));
+var connectionString = builder.Configuration["ConnectionStrings:FinanceDB"];
+Console.WriteLine($"Connection string: {connectionString}");
 
 if (string.IsNullOrEmpty(connectionString))
 {
@@ -47,102 +13,19 @@ if (string.IsNullOrEmpty(connectionString))
 }
 
 builder.Services.AddDbContext<Context>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("FinanceDB"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("FinanceDB"))
-    ));
-
-// Adding Authentication and Authorization
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme; 
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; 
-})
-    .AddJwtBearer("CustomJWT", opt =>
-    {
-        opt.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-        };
-    })
-    .AddJwtBearer("GoogleJWT", options =>
-    {
-        options.Authority = "https://accounts.google.com";
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuers = new[] { "https://accounts.google.com", "accounts.google.com" },
-            ValidateAudience = true,
-            ValidAudiences = new[]
-            {
-                builder.Configuration["Authentication:Google:ClientId"]
-            }
-        };
-    });
-
-builder.Services.AddAuthorization();
-
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
-builder.Services.AddScoped<ICatalogRepository, CatalogRepository>();
-builder.Services.AddScoped<TransactionService>();
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
 
 var app = builder.Build();
 
-//using (var scope = app.Services.CreateScope())
-//{
-//    var services = scope.ServiceProvider;
-//    var env = services.GetRequiredService<IWebHostEnvironment>();
-//
-//    if(env.IsDevelopment())
-//    {
-//        var context = services.GetRequiredService<Context>();
-//
-//        // DB exists
-//        context.Database.Migrate();
-//
-//        // Seed data
-//        SeedData.Initialize(context);
-//    }
-//}
+app.MapGet("/", () => "¡API corriendo desde Railway!");
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseCors("AllowSpecificOrigin");
-app.UseHttpsRedirection();
-app.UseRouting(); 
-
-//app.UseCors(AllowSpecificOrigins);
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
-
-//app.Run();
 try
 {
     app.Run();
 }
 catch (Exception ex)
 {
-    Console.WriteLine("Excepción al iniciar la app:");
+    Console.WriteLine("Error al ejecutar la app:");
     Console.WriteLine(ex.ToString());
 }
