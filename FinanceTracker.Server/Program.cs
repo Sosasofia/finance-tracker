ac6644be51986c08f2f5efc4488101efb79b067b
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-04-18 21:38:55 -0300

    Update

--- FinanceTracker.Server/Program.cs
+++ FinanceTracker.Server/Program.cs
@@ -3,13 +3,7 @@ using FinanceTracker.Server.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration["ConnectionStrings:FinanceDB"];
-Console.WriteLine($"Connection string: {connectionString}");
 
-
-builder.Services.AddDbContext<Context>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
-);
 
 var app = builder.Build();
 
