7c3dce5bc064fea7f8cf93a1872a0438b0c81fa5
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-06-24 16:18:30 -0300

    Add User table migration and update entity relationships
    - Added migration to create the `User` table with properties like `Id`, `Email`, `Name`, `Username`, `ProfilePictureUrl`, `Password`, `Provider`, `Role`, `CreatedAt`, and `LastLoginAt`.
    - Modified `User` entity class to include new properties and default values for `Provider` ("local") and `Role` ("User").

--- FinanceTracker.Server/Models/Entities/User.cs
+++ FinanceTracker.Server/Models/Entities/User.cs
@@ -3,8 +3,17 @@
     public class User
     {
         public Guid Id { get; set; }
+
+        public string? Email { get; set; } = null!;
+        public string? Name { get; set; } 
         public string? Username { get; set; }
+        public string? ProfilePictureUrl { get; set; }
         public string? Password { get; set; }
+
+        public string Provider { get; set; } = "local";
         public string? Role { get; set; } = "User";
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastLoginAt { get; set; }
     }
 }
