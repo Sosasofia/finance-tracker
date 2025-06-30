402344b6940f37478ff45a59862c107e4a0a3be8
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-06-30 17:54:38 -0300

    Add loading indicator to login button and improve loading state management

--- financetracker.client/src/app/shared/components/loading/loading.component.ts
+++ financetracker.client/src/app/shared/components/loading/loading.component.ts
@@ -5,13 +5,14 @@ import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 @Component({
   selector: "app-loading",
   template: `
-    <div *ngIf="loading" class="overlay">
-      <mat-spinner [diameter]="50"></mat-spinner>
+    <div *ngIf="loading" class="overlay loading-spinner">
+      <mat-spinner [diameter]="diameter"></mat-spinner>
     </div>
   `,
-  styleUrl: "./loading.component.css",
+  styleUrls: ["./loading.component.css"],
   imports: [CommonModule, MatProgressSpinnerModule],
 })
 export class LoadingComponent {
   @Input() loading = false;
+  @Input() diameter = 50; 
 }
