1ccd8b3577722cbaa14718fe437060455a4b7ff8
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2025-07-15 16:47:47 -0300

    Added `LastModifiedAt` and `ReimbursementId` properties to the `Transaction` entity

--- FinanceTracker.Server/Data/Migrations/20250715143107_UpdateTransactionEntity.Designer.cs
+++ FinanceTracker.Server/Data/Migrations/20250715143107_UpdateTransactionEntity.Designer.cs
@@ -4,16 +4,19 @@ using FinanceTracker.Server.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace FinanceTracker.Server.Migrations
+namespace FinanceTracker.Server.Data.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250715143107_UpdateTransactionEntity")]
+    partial class UpdateTransactionEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- FinanceTracker.Server/Models/Entities/Transaction.cs
+++ FinanceTracker.Server/Models/Entities/Transaction.cs
@@ -6,18 +6,23 @@ namespace FinanceTracker.Server.Models.Entities
     {
         public Guid Id { get; set; }
         public decimal Amount { get; set; }
-        public string Name { get; set; }
-        public string? Description { get; set; }
+        public string Name { get; set; } = "";
         public DateTime Date { get; set; }
-        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+        public string? Description { get; set; }
         public string? Notes { get; set; }
         public string? ReceiptUrl { get; set; }
         public TransactionType Type { get; set; } = TransactionType.Expense;
+
+        // Audit properties
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? LastModifiedAt { get; set; }
         public bool IsDeleted { get; set; } = false;
         public DateTime? DeletedAt { get; set; }
+
         // Foreign keys
         public Guid? CategoryId { get; set; }
         public Guid? PaymentMethodId { get; set; }
+        public Guid? ReimbursementId { get; set; }  
         public Guid UserId { get; set; }
 
         // Nav properties
